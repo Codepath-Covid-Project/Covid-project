cd63229f6f48c4cb8732fea567cf8fd52e6a6a8a
Justin Nguyen <justnguyen1@gmail.com>
2020-11-30 19:11:38 -0800

    login view

--- CovidCapacity/ViewController.swift
+++ CovidCapacity/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signInButton: UIButton!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        signInButton.layer.borderWidth = 1.0
+        signInButton.layer.borderColor = UIColor(red: 0, green: 0.478431, blue: 1, alpha: 1).cgColor
+        signUpButton.layer.borderWidth = 1.0
+        signUpButton.layer.borderColor = UIColor(red: 0, green: 0.478431, blue: 1, alpha: 1).cgColor
     }
 
 
