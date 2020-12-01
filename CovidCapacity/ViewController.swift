10a7c3fc354b7b29fa51d20a5f5929348231c887
Justin Nguyen <52771821+justnguyen1@users.noreply.github.com>
2020-11-30 18:48:38 -0800

    Add files via upload

---
+++ CovidCapacity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidCapacity
+//
+//  Created by Justin on 11/30/20.
+//  Copyright © 2020 CovidCapacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
