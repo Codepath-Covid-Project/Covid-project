269e2c012405fa5407b9e3f7467ab0db79d5a7c2
Emma Yuan <emmayyw@gmail.com>
2020-12-10 03:17:38 -0800

    converted address into coordinates and added app icon

--- CovidCapacity/ProfileScreenViewController.swift
+++ CovidCapacity/ProfileScreenViewController.swift
@@ -7,25 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ProfileScreenViewController: UIViewController {
     @IBOutlet weak var image: UIImageView!
-    @IBOutlet weak var name: UITextField!
-    @IBOutlet weak var addressLine: UITextField!
-    @IBOutlet weak var aptNum: UITextField!
-    @IBOutlet weak var city: UITextField!
-    @IBOutlet weak var state: UITextField!
-    @IBOutlet weak var zipCode: UITextField!
-    @IBOutlet weak var maxCapacity: UITextField!
-    @IBOutlet weak var edit: UIButton!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var addressLineField: UITextField!
+    @IBOutlet weak var aptNumField: UITextField!
+    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var stateField: UITextField!
+    @IBOutlet weak var zipCodeField: UITextField!
+    @IBOutlet weak var maxCapacityField: UITextField!
+    @IBOutlet weak var editButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        edit.layer.borderWidth = 1.0
-        edit.layer.borderColor = UIColor(red: 0, green: 0.478431, blue: 1, alpha: 1).cgColor
-        edit.layer.borderWidth = 1.0
-        edit.layer.borderColor = UIColor(red: 0, green: 0.478431, blue: 1, alpha: 1).cgColor
+        editButton.layer.borderWidth = 1.0
+        editButton.layer.borderColor = UIColor(red: 0, green: 0.478431, blue: 1, alpha: 1).cgColor
+        editButton.layer.borderWidth = 1.0
+        editButton.layer.borderColor = UIColor(red: 0, green: 0.478431, blue: 1, alpha: 1).cgColor
+                
+        let addressLine = addressLineField.text!
+        let city = cityField.text!
+        let state = stateField.text!
+        let zipCode = zipCodeField.text!
+        let address = addressLine + ", " + city + ", " + state + " " + zipCode
+        // print(address)
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) {
+            placemarks, error in
+            let placemark = placemarks?.first
+            let lat = placemark?.location?.coordinate.latitude
+            let lon = placemark?.location?.coordinate.longitude
+            print("Lat: \(lat), Lon: \(lon)")
+        }
     }
     
 
