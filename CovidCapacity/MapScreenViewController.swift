bf9dee844120c7db5d783358ce8ff971579d3a78
Justin Nguyen <justnguyen1@gmail.com>
2020-12-11 18:31:03 -0800

    map search bar

--- CovidCapacity/MapScreenViewController.swift
+++ CovidCapacity/MapScreenViewController.swift
@@ -8,28 +8,79 @@
 
 import UIKit
 import Mapbox
+import MapboxSearch
+import MapboxSearchUI
 
-class MapScreenViewController: UIViewController {
+class MapScreenViewController: UIViewController, LocationProvider {
+    
+    lazy var searchController = MapboxSearchController(locationProvider: self)
+    
+    func currentLocation() -> CLLocationCoordinate2D? { mapboxSFOfficeCoordinate }
+     
+    lazy var panelController = MapboxPanelController(rootViewController: searchController)
+     
+    let mapView = MGLMapView()
+     
+    let mapboxSFOfficeCoordinate = CLLocationCoordinate2D(latitude: 37.7911551, longitude: -122.3966103)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let url = URL(string: "mapbox://styles/mapbox/streets-v11")
-        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
+        mapView.frame = view.bounds
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
+        mapView.showsUserLocation = true
+         
+        mapView.setCenter(mapboxSFOfficeCoordinate, zoomLevel: 15, animated: false)
         view.addSubview(mapView)
+         
+        searchController.delegate = self
+        addChild(panelController)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func showAnnotation(_ annotations: [MGLAnnotation], isPOI: Bool) {
+        guard !annotations.isEmpty else { return }
+         
+        if let existingAnnotations = mapView.annotations {
+            mapView.removeAnnotations(existingAnnotations)
+        }
+        mapView.addAnnotations(annotations)
+         
+        if annotations.count == 1, let annotation = annotations.first {
+            mapView.setCenter(annotation.coordinate, zoomLevel: 15, animated: true)
+        } else {
+            mapView.showAnnotations(annotations, animated: true)
+        }
     }
-    */
+}
 
+extension MapScreenViewController: SearchControllerDelegate {
+    func categorySearchResultsReceived(results: [SearchResult]) {
+        let annotations = results.map { searchResult -> MGLPointAnnotation in
+            let annotation = MGLPointAnnotation()
+            annotation.coordinate = searchResult.coordinate
+            annotation.title = searchResult.name
+            annotation.subtitle = searchResult.address?.formattedAddress(style: .medium)
+            return annotation
+        }
+     
+        showAnnotation(annotations, isPOI: false)
+    }
+     
+    func searchResultSelected(_ searchResult: SearchResult) {
+        let annotation = MGLPointAnnotation()
+        annotation.coordinate = searchResult.coordinate
+        annotation.title = searchResult.name
+        annotation.subtitle = searchResult.address?.formattedAddress(style: .medium)
+         
+        showAnnotation([annotation], isPOI: searchResult.type == .POI)
+    }
+     
+    func userFavoriteSelected(_ userFavorite: FavoriteRecord) {
+        let annotation = MGLPointAnnotation()
+        annotation.coordinate = userFavorite.coordinate
+        annotation.title = userFavorite.name
+        annotation.subtitle = userFavorite.address?.formattedAddress(style: .medium)
+         
+        showAnnotation([annotation], isPOI: true)
+    }
 }
--- Podfile
+++ Podfile
@@ -8,6 +8,7 @@ target 'CovidCapacity' do
   # Pods for CovidCapacity
   pod 'Mapbox-iOS-SDK', '~> 6.3.0'
   pod 'Parse'
+  pod 'MapboxSearchUI', ">= 1.0.0-beta.2", "< 2.0"
   
   target 'CovidCapacityTests' do
     inherit! :search_paths
